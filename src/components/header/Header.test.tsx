cb36afc39f0938bbfc1ecafc1a6755ed9a9bba52
Diana Khizhnova <khnizovad@gmail.com>
2025-07-19 12:49:05 +0500

    feat: add test 1 for header

---
+++ src/components/header/Header.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { Header } from './Header';
+import { messages } from './messages';
+
+describe('Header component', () => {
+  it('renders the header with correct title', () => {
+    render(<Header />);
+    const header = screen.getByRole('banner');
+    expect(header).toBeInTheDocument();
+    expect(screen.getByText(messages.appTitle)).toBeInTheDocument();
+  });
+});
