97feb08a30374af431da617746d8711586c06f4e
Diana Khizhnova <khnizovad@gmail.com>
2025-07-19 12:48:03 +0500

    feat: add test 1 for footer

---
+++ src/components/footer/Footer.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { Footer } from './Footer';
+import { messages } from './messages';
+
+describe('Footer component', () => {
+  it('renders the footer with correct text', () => {
+    render(<Footer />);
+    const footer = screen.getByRole('contentinfo');
+    expect(footer).toBeInTheDocument();
+    expect(screen.getByText(messages.bottomTitle)).toBeInTheDocument();
+  });
+});
