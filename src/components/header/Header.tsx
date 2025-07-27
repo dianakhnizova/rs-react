9638e4c0caeebf28685db8ffb589ea3ac76b78d0
Diana Khizhnova <khnizovad@gmail.com>
2025-07-27 16:54:05 +0500

    style: update style for header, button and BookCard

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,16 @@
+import { NavLinks } from './components/NavLinks';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 import { messages } from './messages';
+import { PagePath } from '@/router/enums';
 
 export const Header = () => {
   return (
     <header className={styles.container}>
-      <h1 className={styles.title}>{messages.appTitle}</h1>
+      <NavLink to={PagePath.root} className={styles.title}>
+        {messages.appTitle}
+      </NavLink>
+      <NavLinks />
     </header>
   );
 };
