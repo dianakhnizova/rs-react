07be88274fceedb6303d5dc4856cd2b0bfe90b2e
Diana Khizhnova <khnizovad@gmail.com>
2025-08-05 18:13:55 +0500

    feat: add favorites to header

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
 import { bookApi } from '@/api/book.api';
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { cartReducer } from './slices/cart/cart.slice';
 
+const reducers = combineReducers({
+  [bookApi.reducerPath]: bookApi.reducer,
+  cart: cartReducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    [bookApi.reducerPath]: bookApi.reducer,
-    cart: cartReducer,
-  },
+  reducer: reducers,
 
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(bookApi.middleware),
