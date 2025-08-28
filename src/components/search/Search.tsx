f9a64c94899898ca3d5dfc5633f17a8de7446f26
Diana Khizhnova <khnizovad@gmail.com>
2025-08-28 19:31:41 +0500

    style: update style for Search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,14 +1,22 @@
 import { InputType } from '@/sources/enums';
 import { Input } from '../input/Input';
 import { messages } from '@/sources/messages';
+import { Button } from '../button/Button';
+import styles from './Search.module.scss';
+
+const handleSearch = () => {};
 
 export const Search = () => {
   return (
-    <Input
-      id={InputType.TEXT}
-      type={InputType.TEXT}
-      placeholder={messages.button.search}
-      isSearch
-    />
+    <div className={styles.container}>
+      <Input
+        id={InputType.TEXT}
+        type={InputType.TEXT}
+        placeholder={messages.button.search}
+        isSearch
+      />
+
+      <Button onClick={handleSearch}>{messages.button.search}</Button>
+    </div>
   );
 };
